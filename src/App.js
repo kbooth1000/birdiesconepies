9b66edee172e87093246af31ba0106bfcd909135
Kyle Booth <kbooth1000@gmail.com>
2019-07-15 07:14:25 -0400

    Make root page temp landing page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Header from "./Header";
 import Nav from "./Nav";
 import HomeContent from "./HomeContent";
 import Footer from "./Footer";
-import Menu from './Menu';
-import FindUs from './FindUs';
-import Contact from './Contact';
+import Menu from "./Menu";
+import FindUs from "./FindUs";
+import Contact from "./Contact";
+import TempHomeContent from "./TempHomeContent";
 
 import "./App.css";
 
 class App extends Component {
   state = {
-    page: 'home'
-  }
+    page: "home"
+  };
 
   render() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Nav />
+    return (
+      <Router>
+        <Route path="/" exact component={TempHomeContent} />
+        <div className="App">
+          <Route
+            render={({ location }) =>
+              location.pathname !== "/" && location.pathname !== "/login" ? (
+                <Header />
+              ) : null
+            }
+          />
+          {/* <Header /> */}
+          <Route
+            render={({ location }) =>
+              location.pathname !== "/" ? <Nav /> : null
+            }
+          />
+          {/* <Nav /> */}
 
-        <main>
-          <Route path="/" exact component={HomeContent} />
-          <Route path="/menu" exact component={Menu} />
-          <Route path="/find-us" exact component={FindUs} />
-          <Route path="/contact" exact component={Contact} />
-        </main>
+          <main>
+            <Route path="/under-construction" exact component={HomeContent} />
+            <Route path="/menu" exact component={Menu} />
+            <Route path="/find-us" exact component={FindUs} />
+            <Route path="/contact" exact component={Contact} />
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
-  );
+          <Footer />
+        </div>
+      </Router>
+    );
   }
 }
 
