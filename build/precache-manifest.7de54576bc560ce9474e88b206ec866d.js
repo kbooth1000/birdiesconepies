7ab25107b0393d1aca7864833513cc1a88c612b6
Kyle Booth <kbooth1000@gmail.com>
2019-07-15 00:13:24 -0400

    Add initial react app

---
+++ build/precache-manifest.7de54576bc560ce9474e88b206ec866d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521ed91d3355fcd233874eb5f2c5352f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae00d6c683f0d0df4baa",
+    "url": "/static/css/main.4c2e32ac.chunk.css"
+  },
+  {
+    "revision": "a5ebe0bf25fea7f554da",
+    "url": "/static/js/2.8ed0239d.chunk.js"
+  },
+  {
+    "revision": "ae00d6c683f0d0df4baa",
+    "url": "/static/js/main.14e7fddd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b0259cbdd977e1dbc8b69e2e108d5cf",
+    "url": "/static/media/cloudline.4b0259cb.svg"
+  },
+  {
+    "revision": "97e2f68fb50aedbee9b487020b55c6c4",
+    "url": "/static/media/cone-pie-01-long-bg.97e2f68f.jpg"
+  },
+  {
+    "revision": "07379462a283b83aba14e53d21f2f6c1",
+    "url": "/static/media/hand-holding-raspberry-pie.07379462.png"
+  },
+  {
+    "revision": "fe6d31b7359bfb2c43d95a839b3dc6ab",
+    "url": "/static/media/logo-with-tagline.fe6d31b7.svg"
+  },
+  {
+    "revision": "6a425d2d74dacb24698bf12ef25213a3",
+    "url": "/static/media/starburst-1.6a425d2d.svg"
+  },
+  {
+    "revision": "4afe0498d1175811ea7f820dce85b604",
+    "url": "/static/media/starburst-2.4afe0498.svg"
+  },
+  {
+    "revision": "ef08dd80223cd95a22681898cc306374",
+    "url": "/static/media/starburst-3.ef08dd80.svg"
+  },
+  {
+    "revision": "63d6c8f9bd2d07b65cd3c0ecfa818dfc",
+    "url": "/static/media/strawberry-cone-pie.63d6c8f9.png"
+  }
+]);
