5f0cd837256e7f6cd9a6ddf0e1a77c0f27566c9e
Kyle Booth <kbooth1000@gmail.com>
2019-07-26 17:20:15 -0400

    Prep for first live; new home page

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -1,26 +1,24 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import raspberryPie from "./assets/hand-holding-raspberry-pie.png";
+import './home.css';
+import raspberryPie from './assets/hand-holding-raspberry-pie.png';
 
 export default function HomeContent() {
   return (
-    <div className="main-content">
-    <h2>
-      <span>Welcome to </span> <span> Birdie's Cone Pies</span></h2>
+    <div className="main-content home">
+      <h2 className="welcome">
+        <span>Welcome to </span> <span> Birdie's Cone Pies</span>
+      </h2>
       <img
         src={raspberryPie}
         alt="hand holding raspberry cone pie"
         className="left-side-pic"
       />
 
-      <Link to="/find-us" style={{
-        height: '10rem',
-        width: '10rem',
-        background: 'url(assets/SVG/starburst-1.svg)', border: '1px solid #f00', display: 'inline-block', fontSize: '1.3rem'
-      }}>
-        Find Our Truck!
-      </Link>
+      <NavLink to="/find-us" className="star-link two">
+        <span>Find Our Truck!</span>
+      </NavLink>
 
       <p>
         <span className="birdie-type">Birdie’s Cone Pies</span> are “grandma’s”
@@ -37,14 +35,12 @@ export default function HomeContent() {
         menu. Homemade apple pies, peach cobbler, chicken pot pies... you get
         the idea. Lots to love.
       </p>
-      <p>
-        <strong>Coming Summer 2019!</strong>
-      </p>
-      {/* <!-- <div className="main-pic">
+      <div style={{ clear: 'both', height: '4rem' }} />
+      <div className="main-pic">
         <h2 aria-hidden="true">
           Hand holding a raspberry cone pie with whipped cream.
         </h2>
-      </div> --> */}<div style={{clear: 'both', height: '4rem'}}></div>
+      </div>
     </div>
   );
 }
