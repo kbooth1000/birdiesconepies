fa64545911629448c8b2cb8a834cdbd510b2b430
Kyle Booth <kbooth1000@gmail.com>
2019-07-30 11:30:02 -0400

    Update HomeContent.js

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -21,19 +21,26 @@ export default function HomeContent() {
       </NavLink>
 
       <p>
-        <span className="birdie-type">Birdie’s Cone Pies</span> are “grandma’s”
-        delicious pie crusts in a cone shape ready to eat on the go.
-      </p>
-      <p>
-        Our pies can be filled with our signature cheesecake, sweet or tangy
-        cream and your choice of fruits, chocolate, caramel, peanut butter...
-        and so much more. Create your own combination.
-      </p>
-      <p>
-        Also look for our fresh fruit and savory pies on our “birds of the week”
-        menu. Homemade apple pies, peach cobbler, chicken pot pies... you get
-        the idea. Lots to love.
-      </p>
+            <span className="birdie-type">Birdie’s Cone Pies</span> are
+            “grandma’s” delicious pie crusts in a cone shape ready to eat on the
+            go.
+          </p>
+
+          <p>
+            Our pies can be filled with our signature cheesecake, sweet or tangy
+            cream and your choice of fruits, chocolate, caramel, peanut
+            butter... and so much more. Create your own combination.
+          </p>
+          <p>
+            Also look for our fresh fruit pies on our "Bird of the Day" menu.
+            Homemade apple pie, mixed berry "Dodo" pie, peach cobblers, ... you
+            get the idea.
+          </p>
+          <p>
+            And for "Little Larks," come build-a-nest with your favorite
+            flavored cream, delicious mix-ins, sprinkled pie crust, and topped
+            with a "birdie." Lots to love.
+          </p>
       <div style={{ clear: 'both', height: '4rem' }} />
       <div className="main-pic">
         <h2 aria-hidden="true">
