47877051ea47f5f5f83d219f13b46708f6096d54
Kyle Booth <kbooth1000@gmail.com>
2019-10-23 02:06:59 -0400

    Make gallery work with pics

--- src/Gallery.js
+++ src/Gallery.js
@@ -10,8 +10,8 @@ export default class Gallery extends Component {
     }
   }
 
-  imageCells = ['1', '2', '3', '4', '5',
-    '6', '7']
+  imageCells = ['1-_DSC2972.jpg', '2-_DSC2973.jpg', '3-_DSC2981.jpg', '4-_DSC2986.jpg', '5-_DSC2988.jpg', '6-_DSC2990.jpg', '9-_DSC2998.jpg', '10-_DSC2999.jpg', '11-_DSC3002.jpg', '12-_DSC3004.jpg', '14-_DSC3018.jpg', '15-_DSC3022.jpg', '21-_DSC3043.jpg', '22-_DSC3045.jpg', '23-_DSC3053.jpg', '24-_DSC3055.jpg', '26-_DSC3063.jpg', '27-_DSC3067.jpg', '30-_DSC3086.jpg', '31-_DSC3092.jpg', '32-_DSC3098.jpg'
+  ]
 
   handleImgClick = imgNum => {
     this.setState({
@@ -26,13 +26,14 @@ export default class Gallery extends Component {
 
           {
             this.imageCells.map(
-              (img, i) => <figure key={i} className={`image ${this.state.expandedImg === i ? "expanded" : ""}`} onClick={() => { this.handleImgClick(i) }}>{img}</figure>
+              (img, i) => <figure key={i} className={`image ${this.state.expandedImg === i ? "expanded" : ""}`} onClick={() => { this.handleImgClick(i) }}
+                style={{ background: `url(/2019-10-22/${img}) no-repeat` }}></figure>
             )
           }
         </div>
 
 
-      </div>
+      </div >
     )
   }
 }
--- src/HomeContent.js
+++ src/HomeContent.js
@@ -20,26 +20,30 @@ export default function HomeContent() {
         <span>Find Our Truck!</span>
       </NavLink>
 
+      <NavLink to="/gallery" className="star-link one">
+        <span>See Photos</span>
+      </NavLink>
+
       <p>
-            <span className="birdie-type">Birdie’s Cone Pies</span> are
-            “grandma’s” delicious pie crusts in a cone shape ready to eat on the
-            go.
+        <span className="birdie-type">Birdie’s Cone Pies</span> are
+        “grandma’s” delicious pie crusts in a cone shape ready to eat on the
+        go.
           </p>
 
-          <p>
-            Our pies can be filled with our signature cheesecake, sweet or tangy
-            cream and your choice of fruits, chocolate, caramel, peanut
-            butter... and so much more. Create your own combination.
+      <p>
+        Our pies can be filled with our signature cheesecake, sweet or tangy
+        cream and your choice of fruits, chocolate, caramel, peanut
+        butter... and so much more. Create your own combination.
           </p>
-          <p>
-            Also look for our fresh fruit pies on our "Bird of the Day" menu.
-            Homemade apple pie, mixed berry "Dodo" pie, peach cobblers, ... you
-            get the idea.
+      <p>
+        Also look for our fresh fruit pies on our "Bird of the Day" menu.
+        Homemade apple pie, mixed berry "Dodo" pie, peach cobblers, ... you
+        get the idea.
           </p>
-          <p>
-            And for "Little Larks," come build-a-nest with your favorite
-            flavored cream, delicious mix-ins, sprinkled pie crust, and topped
-            with a "birdie." Lots to love.
+      <p>
+        And for "Little Larks," come build-a-nest with your favorite
+        flavored cream, delicious mix-ins, sprinkled pie crust, and topped
+        with a "birdie." Lots to love.
           </p>
       <div style={{ clear: 'both', height: '4rem' }} />
       <div className="main-pic">
